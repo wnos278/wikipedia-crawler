a7902c7ee2f16b595571a4cc8104c15ac740d31c
sontdc <37098981+wnos278@users.noreply.github.com>
2024-08-05 00:15:17 +0700

    Edit README and app.py

---
+++ README.md
@@ -0,0 +1,11 @@
+### Design link: https://drive.google.com/file/d/1mVG3wiLRrh_q3Xcjr9E1gK4Yf_R5zmkM/view?usp=sharing
+
+
+### Edit seeds.txt file 
+### Edit .env file
+
+
+## $ conda create --name venv python=3.11
+## $ conda activate venv
+## $ pip install -r requirements.txt
+## $ python app.py
--- src/crawler.py
+++ src/crawler.py
@@ -264,25 +264,3 @@ def get_seeds():
     seeds = f.read().strip().split('\n')
     return seeds 
 
-
-
-# Flow 
-seeds = get_seeds() 
-next_page = extract_next_link()
-while next_page:
-    current_page = next_page 
-
-    # Start 
-    raw_html = download_page(current_page)
-    list_links = extract_list_data(raw_html)
-
-
-
-    # Duy trì vòng lặp
-    next_page = extract_next_link() 
-
-print (web_crawl())
-
-t1 = time.time()
-total_time = t1-t0
-print(total_time)
